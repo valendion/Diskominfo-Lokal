c7610caef44699edad6225a9163d27a2ffc3add4
valendion <valendion874@gmail.com>
2024-12-20 00:41:55 +0800

    feat(home): update setting _worker.ts

--- public/_worker.ts
+++ public/_worker.ts
@@ -1,5 +1,5 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "./build/server";
+import * as build from "../build/server";
 
 interface Env {
   ASSETS: Fetcher;
