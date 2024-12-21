a9f548aef3daa54f55e74e75e2a4062cd0c0ae02
valendion <valendion874@gmail.com>
2024-12-21 18:00:24 +0800

    feat(home): un comment index

---
+++ .wrangler/tmp/pages-TyFhuP/functionsRoutes-0.05304757486168632.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ____path___ts_onRequest } from "/home/valendion/user/coding/fe/remixjs/diskominfolocal/functions/[[path]].ts"
+
+export const routes = [
+    {
+      routePath: "/:path*",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [____path___ts_onRequest],
+    },
+  ]
